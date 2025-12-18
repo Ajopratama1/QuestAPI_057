9d8f20dccab48e8ad4f9151dba6f702f869864f5
Ajopratama1 <a.ajopratama01@gmail.com>
2025-12-18 22:44:32 +0700

    mengganti Greeting dengan DataSiswaApp pada MainActivity.kt dan menghapus kode Greeting yang tidak terpakai

--- app/src/main/java/com/example/pam_18desember/MainActivity.kt
+++ app/src/main/java/com/example/pam_18desember/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pam_18desember.ui.theme.PAM_18DESEMBERTheme
+import com.example.pam_18desember.uicontroller.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +21,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             PAM_18DESEMBERTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PAM_18DESEMBERTheme {
-        Greeting("Android")
     }
 }
