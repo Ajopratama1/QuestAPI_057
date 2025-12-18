d95b560d3f53b1d80e2699dfbb3eb7f1878063ea
Ajopratama1 <a.ajopratama01@gmail.com>
2025-12-18 22:40:22 +0700

    menambahkan file DestinasiEntry.kt untuk rute navigasi entry siswa

---
+++ app/src/main/java/com/example/pam_18desember/uicontroller/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.pam_18desember.uicontroller.route
+
+import com.example.pam_18desember.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
