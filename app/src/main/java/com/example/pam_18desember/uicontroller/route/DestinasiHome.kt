581c1b24a228206fa33c57c0ead87634fef133c2
Ajopratama1 <a.ajopratama01@gmail.com>
2025-12-18 22:41:00 +0700

    menambahkan file DestinasiHome.kt untuk definisi rute navigasi halaman utama

---
+++ app/src/main/java/com/example/pam_18desember/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.pam_18desember.uicontroller.route
+
+import com.example.pam_18desember.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
