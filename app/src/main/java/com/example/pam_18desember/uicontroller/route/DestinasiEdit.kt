629b4ccd90d56b0498c7f6c5474e062d0282b895
Ajopratama1 <a.ajopratama01@gmail.com>
2025-12-30 21:12:01 +0700

    menambahkan object DestinasiEdit untuk konfigurasi navigasi edit data siswa

--- app/src/main/java/com/example/pam_18desember/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/pam_18desember/uicontroller/route/DestinasiEdit.kt
@@ -1,2 +1,10 @@
 package com.example.pam_18desember.uicontroller.route
 
+import com.example.pam_18desember.R
+
+object DestinasiEdit : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
