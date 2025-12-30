ae92c1ea2d430c3f5335e43a88454a0b8991c6dc
Ajopratama1 <a.ajopratama01@gmail.com>
2025-12-30 21:14:42 +0700

    menambahkan DetailViewModel dan EditViewModel pada PenyediaViewModel

--- app/src/main/java/com/example/pam_18desember/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pam_18desember/viewmodel/provider/PenyediaViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.pam_18desember.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.pam_18desember.repositori.AplikasiDataSiswa
+import com.example.pam_18desember.viewmodel.DetailViewModel
+import com.example.pam_18desember.viewmodel.EditViewModel
 import com.example.pam_18desember.viewmodel.EntryViewModel
 import com.example.pam_18desember.viewmodel.HomeViewModel
 
@@ -14,9 +17,19 @@ fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
-        initializer { HomeViewModel(aplikasiDataSiswa().container
-            .repositoryDataSiswa) }
-        initializer { EntryViewModel(aplikasiDataSiswa().container
-            .repositoryDataSiswa) }
+        initializer { HomeViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
+        initializer { EntryViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
     }
 }
