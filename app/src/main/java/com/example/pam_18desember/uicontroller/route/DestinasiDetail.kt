fb6b8789885553544ac5e97e4aa5a96e16bfb092
Ajopratama1 <a.ajopratama01@gmail.com>
2025-12-30 21:11:45 +0700

    menambahkan dependensi material3 dan mendefinisikan rute navigasi untuk DestinasiDetail

--- app/src/main/java/com/example/pam_18desember/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/pam_18desember/uicontroller/route/DestinasiDetail.kt
@@ -1,2 +1,10 @@
 package com.example.pam_18desember.uicontroller.route
 
+import com.example.pam_18desember.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
